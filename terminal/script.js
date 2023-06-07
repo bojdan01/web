5c71aeb3c72dd20d1732b10819f4d27aff854288
bojdan01 <125464015+bojdan01@users.noreply.github.com>
2023-06-07 17:03:17 +0200

    Update script.js

--- terminal/script.js
+++ terminal/script.js
@@ -1,31 +1,34 @@
 $("#terminal").terminal(async function (command, terminal) {
     try {
-        const prompt = `you are a helpful, knowledge sharing chatbot. I say: ${command}. You reply:`
+        const prompt = `you are a helpful, knowledge sharing chatbot. I say: ${command}. You reply:`;
         const response = await fetch(
             `https://api.openai.com/v1/completions`,
             {
-                body: JSON.stringify({ "model": "text-davinci-003", "prompt": prompt, "temperature": 0.86, "max_tokens": 256 }),
                 method: "POST",
                 headers: {
-                    "content-type": "application/json",
-                    Authorization: "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",
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer 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",
                 },
+                body: JSON.stringify({ "model": "text-davinci-003", "prompt": prompt, "temperature": 0.86, "max_tokens": 256 })
             }
-        ).then((response) => {
-            if (response.ok) {
-                response.json().then((json) => {
-                    terminal.echo(json.choices[0].text.trim());
-                });
-            }
-        });
+        );
+
+        if (response.ok) {
+            const json = await response.json();
+            terminal.echo(json.choices[0].text.trim());
+        } else {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
 
         console.log("Completed!");
-    } catch (err) { console.error(`Error: ${err}`) }
+    } catch (err) {
+        console.error(`Error: ${err}`);
+    }
 },
-    {
-        greetings: 'GPT-3 Chatbot v0.1',
-        name: 'gpt3_demo',
-        height: 400,
-        width: 800,
-        prompt: '> '
-    });
+{
+    greetings: 'GPT-3 Chatbot v0.1',
+    name: 'gpt3_demo',
+    height: 400,
+    width: 800,
+    prompt: '> '
+});
