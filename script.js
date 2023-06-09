d85d8b6a5c837214b1f43cea2ce823e6f42e751f
Matycz07 <83537224+Matycz07@users.noreply.github.com>
2023-06-09 09:21:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function Ai(){
  window.location.href = "/Ai/index.html";   
 }
+setInterval(()=>{
+    let cas = new Date()
+    document.querySelector(".hodiny").innerHTML = cas.getHours()+":"+cas.getMinutes()+":"+cas.getSeconds()
+})
