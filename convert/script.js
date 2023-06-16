3e29173b4679da0a1c139c8b113716834fccc03a
bojdan01 <125464015+bojdan01@users.noreply.github.com>
2023-06-16 16:48:31 +0200

    Update script.js

--- convert/script.js
+++ convert/script.js
@@ -24,4 +24,12 @@ let rep2 = setInterval(()=>{
         clearInterval(rep2)
     }
 })
-
+document.addEventListener("visibilitychange", (event) => {
+  if (document.visibilityState == "visible") {
+    document.querySelector("title").innerHTML = "Bojdan.cz";
+    document.querySelectorAll("link")[0].href = '/image/letter-b.ico'
+  } else {
+    document.querySelector("title").innerHTML = "Come back";
+    document.querySelectorAll("link")[0].href = '/image/smile.png'
+  }
+});
