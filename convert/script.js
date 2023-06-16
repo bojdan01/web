451ff892fd78dd1f37cf7b0bed6fe5ab441e10e7
bojdan01 <125464015+bojdan01@users.noreply.github.com>
2023-06-16 12:03:15 +0200

    Update script.js

--- convert/script.js
+++ convert/script.js
@@ -1,9 +1,9 @@
-// Získání odkazu na element <a>
-var odkaz = document.querySelector('a');
+// Získání odkazu na element s ID 'file'
+var element = document.getElementById('file');
 
 // Kontrola, zda atribut href existuje
-if (odkaz.hasAttribute('href')) {
-    console.log('Element <a> obsahuje atribut href.');
+if (element.hasAttribute('href')) {
+    console.log("Element s ID 'file' obsahuje atribut href.");
 } else {
-    console.log('Element <a> neobsahuje atribut href.');
+    console.log("Element s ID 'file' neobsahuje atribut href.");
 }
