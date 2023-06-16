cf989835995af2b3dc138427eacf69b6568097a1
Matycz07 <83537224+Matycz07@users.noreply.github.com>
2023-06-16 12:46:38 +0200

    Update script.js

--- convert/script.js
+++ convert/script.js
@@ -2,8 +2,26 @@
 var element = document.getElementById('file');
 
 // Kontrola, zda atribut href existuje
-if (element.hasAttribute('href')) {
-    console.log("Element s ID 'file' obsahuje atribut href.");
-} else {
-    console.log("Element s ID 'file' neobsahuje atribut href.");
-}
+let rep = setInterval(()=>{
+    if (element.hasAttribute('href')) {
+        element.style = `
+        display: block;
+        position: absolute;
+        transform: translateX(-50%) translateY(-50%);
+        left: 50%;
+        top: 50%;
+        `
+        document.querySelector(".tet").style.display = "none"
+        clearInterval(rep)
+    }
+})
+let rep2 = setInterval(()=>{
+    if(document.querySelectorAll("form")[0].style.display=="none"){
+        let d = document.createElement("div")
+        d.innerHTML = '<div class="custom-loader"></div>'
+        d.className = "tet"
+        document.body.appendChild(d)
+        clearInterval(rep2)
+    }
+})
+
