6dc6c1ade92e89ff46c2c296d90411066ce945a5
bojdan01 <125464015+bojdan01@users.noreply.github.com>
2023-06-17 12:43:43 +0200

    Update script.js

--- convert/script.js
+++ convert/script.js
@@ -10,6 +10,16 @@ let rep = setInterval(()=>{
         transform: translateX(-50%) translateY(-50%);
         left: 50%;
         top: 50%;
+         font-family: arial;
+  color: #14396A !important;
+  font-size: 14px;
+  text-shadow: 1px 1px 0px #7CACDE;
+  box-shadow: 1px 1px 1px #BEE2F9;
+  padding: 10px 25px;
+  border-radius: 10px;
+  border: 2px solid #3866A3;
+  background: #63B8EE;
+  background: linear-gradient(to top, #63B8EE, #468CCF);
         `
         document.querySelector(".tet").style.display = "none"
         clearInterval(rep)
