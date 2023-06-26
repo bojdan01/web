bf6cb05909222c6d118053b952ccfba48131bb81
bojdan01 <125464015+bojdan01@users.noreply.github.com>
2023-06-26 11:01:59 +0200

    Update script.js

--- ConvertTest/script.js
+++ ConvertTest/script.js
@@ -15,9 +15,9 @@
  document.addEventListener("visibilitychange", (event) => {
             if (document.visibilityState == "visible") {
             document.querySelector("title").innerHTML = "Bojdan.cz";
-            document.querySelectorAll("link")[0].href = '/image/letter-b.ico'
+            document.querySelectorAll("link")[1].href = '/image/letter-b.ico'
             } else {
             document.querySelector("title").innerHTML = "Come back";
-            document.querySelectorAll("link")[0].href = '/image/smile.png'
+            document.querySelectorAll("link")[1].href = '/image/smile.png'
             }
         });
