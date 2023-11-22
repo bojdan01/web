46116dcd826cfeab74f0c87d68a5b78b63fd6869
bojdan01 <bogdanbojda12@gmail.com>
2023-11-22 16:39:11 +0100

    0

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function pohyb(a,b,c,d){
-    let e = "vmin"
-    document.querySelector(".obsah").style.marginLeft = a+e
-    document.querySelector(".konvertor").style.marginLeft = b+e
-    document.querySelector(".informace").style.marginLeft = c+e
-    document.querySelector(".autori").style.marginLeft = d+e
-}
 
 cas()
 setInterval(()=>{
@@ -24,4 +17,40 @@ xhttp.send();
 } catch (error) {
     
 }
+}
+
+var windowWidth;
+
+function getWindowDimensions() {
+    return {
+        width: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+        height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+    };
+}
+
+function updateContent(windowWidth) {
+    windowWidth = getWindowDimensions().width;
+    return windowWidth;
+}
+window.onload = function () {
+    updateContent();
+};
+
+window.addEventListener('resize', function () {
+    updateContent();
+});
+
+function pohyb(a, b, c, d,) {
+    let e = "vmin";
+        document.querySelector(".obsah").style.marginLeft = a + e;
+        document.querySelector(".konvertor").style.marginLeft = b + e;
+        document.querySelector(".informace").style.marginLeft = c + e;
+        document.querySelector(".autori").style.marginLeft = d + e;
+}
+function pohybMin(a, b, c, d,) {
+    let e = "vmin";
+        document.querySelector(".obsah").style.marginTop = a + e;
+        document.querySelector(".konvertor").style.marginTop = b + e;
+        document.querySelector(".informace").style.marginTop = c + e;
+        document.querySelector(".autori").style.marginTop = d + e;
 }
